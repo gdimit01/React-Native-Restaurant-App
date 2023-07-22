d02458cbc6605eae41b249d6b68ea89f1cb6ce03
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-07-22 21:59:39 +0100

    flatlist updated

--- src/features/restaurants/components/screens/restaurants.screens.js
+++ src/features/restaurants/components/screens/restaurants.screens.js
@@ -2,38 +2,43 @@ import React from "react";
 import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../restaurant-info-card-component";
-import { SafeAreaView, StatusBar } from "react-native";
+import { SafeAreaView, StatusBar, FlatList, View, Text } from "react-native";
 import { Card } from "react-native-paper";
+import { Spacer } from "../spacer/spacer.component";
 
-
-constRestaurantCard = styled(Card)`
-background-color: ${(props) => props.theme.colors.bg.primary};
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
-// 
 const StyledSafeArea = styled(SafeAreaView)`
   flex: 1;
   margin-top: ${StatusBar.currentHeight};
 `;
 
-//Search Container
 const SearchView = styled.View`
-  padding: ${props => props.theme.space[3]};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
-//The list of restaurants
-const ListView = styled.View`
-  flex: 1;
-  padding: ${props => props.theme.space[3]};
-`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+  keyExtractor: (item) => item.name,
+  showsVerticalScrollIndicator: true,
+})``;
 
 export const RestaurantsScreen = () => (
   <StyledSafeArea>
     <SearchView>
       <Searchbar />
     </SearchView>
-    <ListView>
-      <RestaurantInfoCard/>
-    </ListView>
+    <RestaurantList
+      data={[{ name: "1" }, { name: "2" }, { name: "3" }]}
+      renderItem={() => (
+        <Spacer position="bottom" size="large">
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
+    />
   </StyledSafeArea>
-)
+);
